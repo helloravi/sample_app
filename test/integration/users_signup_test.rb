68877eb220819a6ce7a6aa4a6122161ca7f6564a
Ravikanth Andhavarapu <raviandkanth@gmail.com>
2014-12-27 05:12:55 +0530

    Finish sign up information

---
+++ test/integration/users_signup_test.rb
@@ -0,0 +1,25 @@
+require 'test_helper'
+
+class UsersSignupTest < ActionDispatch::IntegrationTest
+  # test "the truth" do
+  #   assert true
+  # end
+  test "invalid signup information" do
+    get signup_path
+    assert_no_difference 'User.count' do
+     post users_path user: {name: "Radhika", email: "foo@invalid",
+                            password: "foo", password_confirmation: "bar"}
+    end
+    assert_template 'users/new'
+  end
+
+  test "valid sign up information" do
+    get signup_path
+    assert_difference 'User.count', 1 do
+      post_via_redirect users_path user: {name: "Radhika", email:"sparkx.radhika@gmail.com",
+                                          password:"sandeep", password_confirmation: "sandeep"}
+    end
+    assert_template 'users/show'
+  end
+
+end
